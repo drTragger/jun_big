8016e6bb4c4d833122c5b51f05fdd200aca290ab
Misha <mishaponomarenko11082001@gmail.com>
2020-12-16 20:58:30 +0200

    controller tasks

---
+++ app/Http/Controllers/Tasks/TasksController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Tasks;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class TasksController extends Controller
+{
+    public function index()
+    {
+        return view('tasks.index');
+    }
+
+    public function create()
+    {
+        echo 'create';
+    }
+}
