724b47e11a084e77f90055f7ae4706f7bf3cc34c
Misha <mishaponomarenko11082001@gmail.com>
2020-12-16 20:56:49 +0200

    view layout default

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laravel Quickstart - Intermediate</title>
+
+    <!-- CSS и JavaScript -->
+</head>
+
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <!-- Содержимое Navbar -->
+    </nav>
+</div>
+
+@yield('content')
+</body>
+</html>
