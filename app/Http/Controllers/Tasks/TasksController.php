22ccc5a7dbf40b4e585ba110e3b4c1e51b39d053
Misha <mishaponomarenko11082001@gmail.com>
2020-12-18 20:20:28 +0200

    controller add action, auth middleware

--- app/Http/Controllers/Tasks/TasksController.php
+++ app/Http/Controllers/Tasks/TasksController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Tasks;
 
 use Illuminate\Http\Request;
-
+use App\Models\Task;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class TasksController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('tasks.index');
@@ -17,5 +24,24 @@ class TasksController extends Controller
     public function create()
     {
         return view('tasks.create');
+    }
+
+    public function add(Request $request)
+    {
+        $this->validate(
+            $request,
+            ['name' => 'required|max:255']
+        );
+        $user = $request->user();
+        $user->tasks()->create(
+            [
+                'name' => $request->name,
+            ]
+        );
+//        $task = new Task();
+//        $task->name = $request->name;
+//        $task->user_id = $user->id;
+//        $task->save();
+        return redirect(route('tasks.index'));
     }
 }
